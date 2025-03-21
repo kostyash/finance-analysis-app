48ca5c52ea8be7bab5b5a1af766a55e6a217fab5
kostyash <kostyash11@gmail.com>
2025-03-21 11:20:28 +0200

    update GitHub Actions workflow for deployment and add configuration for API and CloudFront

---
+++ frontend/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiGateway: {
+      URL: process.env.REACT_APP_API_URL || '',
+    },
+    cloudfront: {
+      URL: process.env.REACT_APP_CLOUDFRONT_URL || '',
+    },
+    environment: process.env.NODE_ENV
+  };
+  
+  export default config;
